07f9fa0d304d61675d822c3661e5d4c9262cc319
Victor Wardi <victorcaetano@live.com>
2018-08-21 14:00:57 -0300

    Creating BaseEntity class

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Person.java
@@ -1,8 +1,18 @@
 package guru.springframework.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
+
+    private Long id;
     private String firstName;
     private  String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -2,7 +2,9 @@ package guru.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
+
+    private Long id;
 
     private LocalDate birthDate;
 
@@ -12,6 +14,14 @@ public class Pet {
 
     public LocalDate getBirthDate() {
         return birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setBirthDate(LocalDate birthDate) {
