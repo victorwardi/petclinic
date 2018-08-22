7593d0afffe61ae27e18c8788c80b579e5cf0101
Victor Wardi <victorcaetano@live.com>
2018-08-22 16:13:50 -0300

    implementing Controller and index page. closes #1

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/controller/PetClinicController.java
@@ -0,0 +1,17 @@
+package guru.springframework.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Victor Wardi - @vwardi - on 22/08/2018.
+ */
+@Controller
+public class PetClinicController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+
+}
