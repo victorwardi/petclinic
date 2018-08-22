08311457c1960cd1404a366f9ab5bf01ee38668f
Victor Wardi <victorcaetano@live.com>
2018-08-22 15:02:51 -0300

    Refactoring Service Interfaces to CrudService Interface. closes #3

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/service/PetService.java
@@ -3,5 +3,4 @@ package guru.springframework.petclinic.service;
 import guru.springframework.petclinic.model.Pet;
 
 
-public interface PetService extends CrudService<Pet, Long>{
-}
+public interface PetService extends CrudService<Pet, Long>{}
