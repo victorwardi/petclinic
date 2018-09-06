e05fb1603273abd3a6ee760f667b2db815579853
Victor Wardi <victorcaetano@live.com>
2018-09-06 13:35:35 -0300

    Adding visit entity. closes #8

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Visit.java
@@ -0,0 +1,37 @@
+package guru.springframework.petclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Victor Wardi - @vwardi - on 06/09/2018.
+ */
+public class Visit extends BaseEntity{
+
+    private LocalDate localDate;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
