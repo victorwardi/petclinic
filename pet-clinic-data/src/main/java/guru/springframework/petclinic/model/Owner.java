1ba96e7f99bf540ae5f74c62045411e00ddeadc8
Victor Wardi <victorcaetano@live.com>
2018-09-06 13:42:38 -0300

    Added Onwer properties. closes #10

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String addresss;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddresss() {
+        return addresss;
+    }
+
+    public void setAddresss(String addresss) {
+        this.addresss = addresss;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
